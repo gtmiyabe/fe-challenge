9dde04641330bdc3e77632d05e6d2a9b5bc90a84
Italo <ifranco@yieldstreet.com>
2020-03-29 12:24:03 -0300

    move to config file and add ellipse svg

--- src/pages/TimelineChallenge/TimelineChallenge.js
+++ src/pages/TimelineChallenge/TimelineChallenge.js
@@ -1,59 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 
-const timeline = [
-  {
-    items: [
-      {
-        description:
-          "In order to invest in the YieldStreet Prism Fund, your YieldStreet investor account setup must be complete, and you must have settled funds for your full allocation in your YieldStreet Wallet.",
-        title: "Get Ready for Your Allocation",
-        active: true
-      },
-      {
-        description:
-          "When the investment window opens, you will submit your desired allocation for the YieldStreet Prism Fund.<br /><br /><b>Note</b>: Funds will <u>not</u> be pulled from your YieldStreet Wallet at this time, but will be reserved for the Fund in your YieldStreet Wallet.",
-        title: "Investment Window Opens",
-        disable: true
-      },
-      {
-        description:
-          "Once the minimum offering requirement of $15 million has been allocated by potential investors, the investment window will close and funds will be withdrawn from your YieldStreet Wallet at this time.",
-        title: "Investment Window Closes"
-      },
-      {
-        description:
-          "Once all investor funds are pulled from the respective Wallets, it will take approximately two business days for initial investments to be made in the Fund and for the Fund to officially go active. Investors should expect to start seeing investment information in their portfolio once the Fund has been live for approximately two weeks.",
-        title: "“Go Active” Date"
-      }
-    ],
-    title: "First Month"
-  },
-  {
-    items: [
-      {
-        description:
-          "Investors will be able to opt into a distribution reinvestment program (DRIP) to automatically reinvest any cash distributions back into the Fund.",
-        title: "Option for Distributions Reinvestment"
-      }
-    ],
-    title: "1-4 Months"
-  },
-  {
-    items: [
-      {
-        description:
-          "Within approximately 15 months after the initial investments go active, and on a quarterly basis thereafter, the Fund may offer to repurchase a limited number of shares from investors subject to approval by the Fund's board of directors and to certain limitations as outlined in the Fund's prospectus.",
-        title: "Option for Limited Liquidity"
-      },
-      {
-        description: "48 months",
-        title: "Fund Termination Date"
-      }
-    ],
-    title: "1-4 Years"
-  }
-];
+import ellipse from "resources/ellipse.svg";
+
+import { timeline } from "./timelineConfig";
 
 const TimelineChallenge = () => {
   let renderRight = true;
@@ -74,7 +24,7 @@ const TimelineChallenge = () => {
                   <ItemContainer left>
                     {!renderRight && (
                       <Item left className="timeline-item">
-                        {/* <Ellipse src={ellipse} left /> */}
+                        <Ellipse src={ellipse} left />
                         <ItemTitle left>{item.title}</ItemTitle>
                         <ItemDescription>{item.description}</ItemDescription>
                       </Item>
@@ -83,7 +33,7 @@ const TimelineChallenge = () => {
                   <ItemContainer right>
                     {renderRight && (
                       <Item right className="timeline-item">
-                        {/* <Ellipse src={ellipse} right /> */}
+                        <Ellipse src={ellipse} right />
                         <ItemTitle right>{item.title}</ItemTitle>
                         <ItemDescription>{item.description}</ItemDescription>
                       </Item>
@@ -100,7 +50,8 @@ const TimelineChallenge = () => {
 };
 
 const Container = styled.div`
-  margin: 0;
+  margin: 0 auto;
+  max-width: 1145px;
 
   :before {
     display: block;
@@ -146,10 +97,9 @@ const InnerContainer = styled.div`
 
 const RowWrapper = styled.div`
   position: relative;
-  max-width: 1145px;
 
   &:before {
-    // content: ${props => `'${props.sectionName}'`};
+    content: ${props => `'${props.sectionName}'`};
     position: absolute;
     font-size: 20px;
     transform: rotate(180deg);
@@ -216,6 +166,13 @@ const ItemTitle = styled.div`
     ${props => props.left && "right: 7px;"}
     top: 28px;
   }
+`;
+
+const Ellipse = styled.img`
+  position: absolute;
+  ${props => props.right && 'left: 0; transform: translateX(-50%);'}
+  ${props => props.left && 'right: 0; transform: translateX(50%);'}
+  top: 22px;
 `;
 
 const ItemDescription = styled.div`
